5a619737e1db2d875a1e17a98a02970a4a5508d5
Nathan Tolbert <ntolbertu85@gmail.com>
2022-03-06 06:56:12 -0500

    03062022_--_proj.py_--_build_click_interface_base
      - added decorator @click.group to entrypoint()
      - added a method for each subcommand in the defined interface
          - each is decorated with @entrypoint.command
          - for now they just echo the location for testing purposes

--- src/project_cli/proj.py
+++ src/project_cli/proj.py
@@ -1,7 +1,40 @@
 import os
+import click
 
-def entrypoint():
-    print('Hello module!')
+@click.group()
+def entrypoint(*opts):
+    if args:
+        args_used_count: int = 0
+        for a in opts:
+            if a == "--version" or a == "-V":
+                click.echo("""
+                    mkproj:
+                        version: 0.1.3_(0.13)
+                        codename: \"WTF\"
+                """)
+        return "0"
+    else:
+        raise ValueError
+
+@entrypoint.command()
+def forPython3():
+    click.echo('Hello Python!')
+
+@entrypoint.command()
+def forJava():
+    click.echo('Hello Java!')
+
+@entrypoint.command()
+def forC():
+    click.echo('Hello C!')
+
+@entrypoint.command()
+def forCpp():
+    click.echo('Hello C++!')
+
+@entrypoint.command()
+def forC():
+    click.echo('Hello Node!')
 
 if __name__== '__main__':
     entrypoint()
