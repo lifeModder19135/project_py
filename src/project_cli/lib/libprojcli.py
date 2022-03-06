b55fdff82c2713d7ae7ad2173ae42b9f5050b019
Nathan Tolbert <ntolbertu85@gmail.com>
2022-03-06 07:16:01 -0500

    03062022_--_libprojcli.py_--_added_class_declarations
      - Added class declarations for:
          - File(os.file)
          - FileBuilder(object)
          - Directory(object)
          - StructConf(object)
      - Added getter implementation for Project
      - Added methods to Project:
          - build_project
          - __make_project_dirs
          - __add_dir_to_path

--- src/project_cli/lib/libprojcli.py
+++ src/project_cli/lib/libprojcli.py
@@ -1,4 +1,29 @@
+
+import os
 import pathlib
+
+class File(os.File):
+    
+    def __init__(self, path:pathlib.Path) -> None:
+        super().__init__(self)
+
+
+class FileBuilder(object):
+    pass
+
+class Directory():
+    pass
+
+class StructConf(object):
+
+    def __init__(self):
+        self._dirs: list[Directory] = list()
+
+    @property
+    def projectdirs():
+        return self._dirs
+
+
 
 class project:
 
@@ -12,11 +37,27 @@ class project:
 
     @property
     def langs(self):
-        return self._langtypes
+        if self._langtypes:
+            if type(self._langtypes) == str:
+                return self._langtypes
+            elif type(self._langtypes) == list:
+                if len(self._langtypes) == 1:
+                    return str(self._langtypes[0])
+                else:
+                    return self._langtypes
+            else:
+                raise TypeError
 
     @rootpath.setter
     def rootpath(self,langs: str):
         self._langtypes = langs
 
+    def build_project(self, location, structure_config):
+        
+        self.__make_project_dirs(location, structure_config)
 
+    def __add_dir_to_path(dir:Directory):
+        pass
 
+    def __make_project_dirs(self, location, structure_config):
+        with os.walk
